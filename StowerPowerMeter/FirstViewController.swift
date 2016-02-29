621dd9bbe5ac0375b96fc2bc2008b0836e11a5b6
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-02-28 17:21:27 -0800

    Initial Commit

---
+++ StowerPowerMeter/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StowerPowerMeter
+//
+//  Created by Andrew Carpenter on 2/28/16.
+//  Copyright (c) 2016 Andrew Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StowerPowerMeter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StowerPowerMeter
+//
+//  Created by Andrew Carpenter on 2/28/16.
+//  Copyright (c) 2016 Andrew Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
